9c381f114282fd668c651c9f3593410a0fee3ed6
xiwei322 <audreywang_2005@outlook.com>
2025-03-29 21:47:38 -0400

    time residual plot

---
+++ 237_Time_Residual_Plot.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+
+
+data <- read.csv("237 Project.csv")
+data$Problem_Set <- factor(data$Problem_Set, levels = c("Set 1", "Set 2", "Set 3", "Set 4"), labels=(c("Simple+Noise", "Simple+No Noise", "Complex+Noise", "Complex+No Noise")))
+
+
+dataModel<-lm(Time_Taken_s~Math_Complexity*Noise_Condition, data=data)
+summary(dataModel)
+anova(dataModel)
+
+
+data$residuals <- resid(dataModel)
+data$standardized.residuals<-rstandard(dataModel)
+data$student<-rstudent(dataModel)
+data$fitted<-fitted(dataModel)
+
+ggplot(data, aes(x=fitted, y=residuals, color=Problem_Set)) +
+  geom_point() +
+  labs(x="Fitted Values", y="Residuals", title="Residual Plot for Time Taken in seconds",
+       color="Condition")
+
